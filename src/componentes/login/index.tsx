8dc8fa194cdf112dbbf5363f34e2190a1a3db49a
Matheus Ribeiro <pessoalsenacsp@gmail.com>
2025-12-10 16:13:18 -0300

    Adicionando validações e regex em register e login

--- src/componentes/login/index.tsx
+++ src/componentes/login/index.tsx
@@ -1,13 +1,44 @@
 import AuthContainer from "@/componentes/ui/AuthContainer";
 import TextField from "@/componentes/ui/TextField";
 import PasswordField from "../ui/PasswordField";
+import React, { useMemo, useState } from "react";
 import { TouchableOpacity, Text, View, Dimensions } from "react-native";
 import { global } from "../ui/style";
-import { login } from "@/componentes/login/login"
+import { login } from "@/componentes/login/login";
 import { useRouter } from "expo-router";
+
+function isValidEmail(email: string) {
+  return /^[^\s@&='!"]@[^\s@&='!"].[^\s@&='!"]$/.test(email);
+}
 
 const RenderLogin = () => {
   const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [touched, setTouched] = useState<{
+    email?: boolean;
+    password?: boolean;
+  }>({});
+
+  const erros = useMemo(() => {
+    const error: Record<string, string> = {};
+    if (touched.email && !email) error.email = "Email obrigatorio";
+    if (touched.password && !password) error.password = "Senha obrigatoria";
+    if (touched.password && password && password.length < 6)
+      error.password = "Minimo de 6 caracteres para a senha";
+    if (touched.email && email && !isValidEmail(email))
+      error.email = "Digite um email válido";
+
+    return error;
+  }, [email, password, touched]);
+
+  const canSubmit =
+    email && password && Object.keys(erros).length === 0 && !loading;
+
+  const handleSubmit = async () => {
+    router.replace("/(tabs)/explorer");
+  };
 
   const { width, height } = Dimensions.get("window");
   return (
@@ -16,23 +47,29 @@ const RenderLogin = () => {
       subtitle="Por favor insira seus dados"
       logo={require("../../../assets/images/profile.png")}
     >
-      
       <View style={[global.content, login.texts]}>
         <TextField
           label="E-mail"
           icon={{ lib: "MaterialIcons", name: "email" }}
           placeholder="user@email.com"
           keyboardType="email-address"
+          value={email}
+          onChangeText={(input) => setEmail(input)}
+          errorText={erros.email}
         />
 
         <PasswordField
           label="Senha"
           icon={{ lib: "MaterialIcons", name: "lock" }}
           placeholder="*********"
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+          errorText={erros.password}
         />
         <TouchableOpacity
           style={[global.primaryButton]}
-          onPress={() => router.push("/(tabs)/reserva")}
+          onPress={handleSubmit}
+          disabled={!canSubmit}
         >
           <Text style={global.primaryButtonText}>Entrar</Text>
         </TouchableOpacity>
--- src/componentes/register/index.tsx
+++ src/componentes/register/index.tsx
@@ -1,14 +1,76 @@
 import AuthContainer from "../ui/AuthContainer";
 import TextField from "../ui/TextField";
 import PasswordField from "../ui/PasswordField";
+import React, { useMemo, useState } from "react";
 import { TouchableOpacity, Text, View, Dimensions } from "react-native";
 import { global } from "../ui/style";
 import { register } from "@/componentes/register/style";
 import { useRouter } from "expo-router";
 import Icon from "react-native-vector-icons/Ionicons";
 
+function isValidRegister(email: string) {
+  return /^[^\s@&='!"]@[^\s@&='!"].[^\s@&='!"]$/.test(email);
+}
+
 const RenderRegister = () => {
   const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [touched, setTouched] = useState<{
+    email?: boolean;
+    password?: boolean;
+    confirmPassword?: boolean;
+  }>({});
+
+  const errors = useMemo(() => {
+    const error: Record<string, string> = {};
+
+    if (touched.email && !email) error.email = "Email obrigatório";
+    if (touched.email && email && !isValidRegister(email))
+      error.email = "Digite um email válido";
+
+    if (touched.password && !password) error.password = "Senha obrigatória";
+
+    if (touched.password && password && password.length < 6)
+      error.password = "Mínimo de 6 caracteres para a senha";
+
+    if (touched.confirmPassword && !confirmPassword)
+      error.confirmPassword = "Confirme sua senha";
+
+    if (
+      touched.confirmPassword &&
+      confirmPassword &&
+      password &&
+      confirmPassword !== password
+    )
+      error.confirmPassword = "As senhas não coincidem";
+
+    return error;
+  }, [email, password, confirmPassword, touched]);
+
+  const canSubmit =
+    email &&
+    password &&
+    confirmPassword &&
+    Object.keys(errors).length === 0 &&
+    !loading;
+
+  const handleSubmit = () => {
+    setTouched({
+      email: true,
+      password: true,
+      confirmPassword: true,
+    });
+
+    if (!canSubmit) return;
+
+    router.replace("/(tabs)/explorer");
+  };
+
   const { width, height } = Dimensions.get("window");
 
   return (
@@ -46,18 +108,35 @@ const RenderRegister = () => {
           icon={{ lib: "MaterialIcons", name: "email" }}
           placeholder="user@email.com"
           keyboardType="default"
+          value={email}
+          onChangeText={setEmail}
+          errorText={errors.email}
         />
 
-        <PasswordField label="Senha" icon={{ lib: "MaterialIcons", name: "lock" }} placeholder="*********" />
+        <PasswordField
+          label="Senha"
+          icon={{ lib: "MaterialIcons", name: "lock" }}
+          placeholder="*********"
+          value={password}
+          onChangeText={setPassword}
+          errorText={errors.password}
+        />
 
         <PasswordField
           label="Confirme sua senha"
           icon={{ lib: "MaterialIcons", name: "lock" }}
           placeholder="*********"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          errorText={errors.confirmPassword}
         />
 
-        <TouchableOpacity style={register.buttonPrimary}>
-          <Text style={register.textContent}>Criar conta </Text>
+        <TouchableOpacity
+          style={[register.buttonPrimary, { opacity: canSubmit ? 1 : 0.5 }]}
+          disabled={!canSubmit}
+          onPress={handleSubmit}
+        >
+          <Text style={register.textContent}>Criar conta</Text>
         </TouchableOpacity>
 
         <View style={{ alignItems: "center", marginTop: height * 0.04 }}>
