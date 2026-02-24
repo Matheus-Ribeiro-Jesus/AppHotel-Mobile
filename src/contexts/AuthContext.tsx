20e50502278b3acdf8a8a673d9857c4b0bc6596b
Matheus-Ribeiro-Jesus <pessoalsenacsp@gmail.com>
2026-02-24 16:07:32 -0300

    criando API login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,14 +1,63 @@
-import { createContext } from "react";
+import React, { createContext, useState, useEffect, useMemo} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_URL } from "@/constants/api";
 
 type AuthContextProps = {
     token: string | null;
     isLoading: boolean;
-    signIn: (username: string, password: string) => Promise<void>;
+    signIn: (email: string, senha: string) => Promise<void>;
     signOut: () => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [token, setToken] = useState<string | null>(null);
+    const [isLoading, setisLoading] = useState(true);
+
+    //Carregar token ao abrir o app 
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const storedToken = await AsyncStorage.getItem("token");
+                if (storedToken) setToken(storedToken);
+            } finally {
+                setisLoading(false);
+            }
+        })();
+
+    }, []);
+
+    async function signIn(email: string, senha: string) {
+        const res = await fetch(`${API_URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, senha }),
+        });
+        // back-end retorna erro {erro: "..."} quando falha
+        if(!res.ok) {
+            const error = await res.json().catch(() => null);
+            throw new Error(error?.message || "Credenciais inválidas");
+        }
+
+        // back-end retorna {token: "..."} quando sucesso
+        const TokenAPI: string = await res.json();
+        await AsyncStorage.setItem("token", TokenAPI);
+        setToken(TokenAPI);
+    }
+
+    async function signOut() {
+        await AsyncStorage.removeItem("token");
+        setToken(null);
+    }
+
+    const value = useMemo(() => ({ token, isLoading, signIn, signOut}),
+        [token, isLoading]
+    );
+    
+
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 
 };
